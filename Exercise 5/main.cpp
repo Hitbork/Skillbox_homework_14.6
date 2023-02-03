464f9a8bf213205398992f7cf284a83531ef49fb
Hitbork <hitbork@yandex.ru>
2023-02-03 09:59:31 +0300

    Exercise 5 - not ready for work release

---
+++ Exercise 5/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// Function for cracking pimples
+void crack(bool pimples[][12], int &counter, int &x1, int &y1, int &x2, int &y2) {
+
+}
+
+
+int main() {
+    // Greetings
+    std::cout << "\nPimples\n\n\n";
+
+
+    // Creating var for pimples statuses
+    bool pimples[12][12];
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            pimples[i][j] = true;
+        }
+    }
+
+
+    //
+    int counter = 0;
+
+}
